c044f04a2c766102aaa4ffa099894c6c0fe071de
Wen Bo <15026630170@163.com>
2018-07-10 16:53:40 +0800

    [column] Column distribution
    Signed-off-by: Wen Bo <15026630170@163.com>

--- ozone/blueprints/column.py
+++ ozone/blueprints/column.py
@@ -21,38 +21,65 @@ def close_db():
     if hasattr(g, 'sqlite3_db'):
         g.sqlite_db.close()
 
-@column_page.route('/<int:page>')
-def show_essay(page=1):
+@column_page.route('/<title>/<int:page>')
+def show_essay(title, page=1):
     if "logged_in" not in session:
         return redirect(url_for("login"))
 
     with app.app_context():
         db = get_db()
         num_per_page = app.config['COLUMN_PER_PAGE']
+        # get all the titles
+        cur = db.cursor().execute("select distinct title from essay order by timestamp desc")
+        titles = [row[0] for row in cur.fetchall()]
+        # print(titles)
 
-        # get total num of essays
-        cur = db.cursor().execute("select count(*) from essay")
-        num = cur.fetchall()[0][0]
-        if (num % num_per_page == 0):
-            max_page = int(num / num_per_page)
+        if (title == "all"):
+            print("title is all")
+            # get total num of essays
+            cur = db.cursor().execute("select count(*) from essay")
+            num = cur.fetchall()[0][0]
+            if (num % num_per_page == 0):
+                max_page = int(num / num_per_page)
+            else:
+                max_page = int((num / num_per_page) + 1)
+            # print(num, max_page)
+
+            if ((page - 1) * num_per_page >= num):
+                print("Illegal page number")
+                page = max_page
+
+            if (page <= 0):
+                print("Illegal page number")
+                page = 1
+
+            cur = db.cursor().execute("select timestamp, owner, title, content from essay order by timestamp desc limit ? offset ?", [num_per_page, (page - 1) * num_per_page])
+            essays = [dict(timestamp=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(row[0])), owner=row[1], title=row[2], content=row[3], collapse_id=row[0]) for row in cur.fetchall()]
+            # print(essays)
         else:
-            max_page = int((num / num_per_page) + 1)
-        # print(num, max_page)
+            # print(title)
+            cur = db.cursor().execute("select count(*) from essay where title = ?", [title])
+            num = cur.fetchall()[0][0]
+            if (num % num_per_page == 0):
+                max_page = int(num / num_per_page)
+            else:
+                max_page = int((num / num_per_page) + 1)
+            # print(num, max_page)
 
-        if ((page - 1) * num_per_page >= num):
-            print("Illegal page number")
-            page = max_page
+            if ((page - 1) * num_per_page >= num):
+                print("Illegal page number")
+                page = max_page
 
-        if (page <= 0):
-            print("Illegal page number")
-            page = 1
+            if (page <= 0):
+                print("Illegal page number")
+                page = 1
 
-        cur = db.cursor().execute("select timestamp, owner, title, content from essay order by timestamp desc limit ? offset ?", [num_per_page, (page - 1) * num_per_page])
-        essays = [dict(timestamp=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(row[0])), owner=row[1], title=row[2], content=row[3], collapse_id=row[0]) for row in cur.fetchall()]
-        # print(essays)
+            cur = db.cursor().execute("select timestamp, owner, title, content from essay where essay.title = ? order by timestamp desc limit ? offset ?", [title, num_per_page, (page - 1) * num_per_page])
+            essays = [dict(timestamp=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(row[0])), owner=row[1], title=row[2], content=row[3], collapse_id=row[0]) for row in cur.fetchall()]
+            # print(essays)
         close_db()
         try:
-            return render_template("show_essay.html", essays=essays, total_page=max_page, current_page=page)
+            return render_template("show_essay.html", essays=essays, total_page=max_page, current_page=page, titles=titles, current_title=title)
         except TemplateNotFound:
             abort(404)
 
